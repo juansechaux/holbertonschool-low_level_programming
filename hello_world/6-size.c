fc7ae2607562fc82337d2e7164cf442657676ee9
juansechaux <juansechaux@hotmail.com>
2023-06-16 15:42:14 -0700

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - size
+ * Return: 0
+ */
+
+int main(void)
+{
+printf("Size of a char: %li byte(s)\n", (unsigned long)sizeof(char));
+printf("Size of a int: %li byte(s)\n", (unsigned long)sizeof(int));
+printf("Size of a long int: %li byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %li byte(s)\n", sizeof(long long int));
+printf("Size of a float: %li byte(s)\n", sizeof(float));
+return (0);
+}
