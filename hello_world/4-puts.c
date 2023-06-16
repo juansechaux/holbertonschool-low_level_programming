1476d22d06bd78ecf78de95215155a54e4274973
juansechaux <juansechaux@hotmail.com>
2023-06-16 14:21:00 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
- *descripcion de programacion a bajo nivel
+ * main - Entry point
  *
+ * Description: "Programming is like building a multilingual puzzle"
  *
+ * return: Always 0 (Success)
  */
 
 int main(void)
