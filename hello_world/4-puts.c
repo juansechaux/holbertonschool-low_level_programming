61d9767ba895ffa8cb6981f210ff63f374610d5c
juansechaux <juansechaux@hotmail.com>
2023-06-16 14:47:07 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
- * Description: "Programming is like building a multilingual puzzle"
- *
- * return: Always 0 (Success)
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-const char *mensaje = "\"Programming is like building a multilingual puzzle";
-puts(mensaje);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
