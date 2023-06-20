25f60178ba600e5494a1845f1888ca4857511178
juansechaux <juansechaux@hotmail.com>
2023-06-20 09:45:02 -0700

    0-positive_or_negative.c

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - funcion de prositivo, negativo
+ * comment - "0. Positive anything is better than negative nothing"
+ * Return:0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("is positive\n");
+else if (n == 0)
+printf("is zero\n");
+	else
+	  printf("is negative\n");
+	return (0);
+}
---
+++ variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+Este es el README del taller de variables_if_else_while
+0-positive_or_negative.c
