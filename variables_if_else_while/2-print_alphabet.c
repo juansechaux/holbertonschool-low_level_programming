06d2c6cb9c954601881cfff6be1c338e5078de5d
juansechaux <juansechaux@hotmail.com>
2023-06-20 11:06:27 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - funcion de prositivo, negativo
+ * comment - "2-print_alphabet.c"
+ * Return:0
+ */
+int main(void)
+{
+char alfabeto;
+for (alfabeto = 'a'; alfabeto <= 'z'; alfabeto++)
+putchar(alfabeto);
+putchar('\n');
+return (0);
+}
