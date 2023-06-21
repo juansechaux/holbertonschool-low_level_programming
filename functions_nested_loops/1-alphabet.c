596ba62bca93f9801a2e1023c59fd0788cab5e46
juansechaux <juansechaux@hotmail.com>
2023-06-21 12:24:18 -0700

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char alfabeto;
+
+	for (alfabeto = 'a'; alfabeto <= 'z'; alfabeto++)
+	{
+		_putchar(alfabeto);
+	}
+	_putchar('\n');
+}
+
