3427151361319d3fd4f094edaf5c0e7f80ede9fd
juansechaux <juansechaux@hotmail.com>
2023-06-22 10:23:21 -0700

    8-24_hours.c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 void print_alphabet(void);
 int _putchar(char c);
 void print_alphabet_x10(void);
@@ -10,7 +9,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-
+void jack_bauer(void);
 
 
 #endif
