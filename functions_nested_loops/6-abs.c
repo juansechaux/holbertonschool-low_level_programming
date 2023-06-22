29181cc529c6101d75e64ca97bebf13877b7ba98
juansechaux <juansechaux@hotmail.com>
2023-06-21 20:38:00 -0700

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * _abs - prueba
+  * @int: prueba
+  * Return: Always 0.
+  */
+int _abs(int r)
+{
+	if (r < 0)
+	{
+	int _abs;
+
+	_abs = r * -1;
+
+	return (_abs);
+	}
+
+	return (r);
+
+}
