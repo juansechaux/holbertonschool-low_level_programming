d9d61e883428c878dc986038fce1618a80f7d4cc
juansechaux <juansechaux@hotmail.com>
2023-06-24 19:31:22 -0700

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -15,12 +15,9 @@ void more_numbers(void)
 		for (num = 0; num <= 14; num++)
 		{
 			if (num >= 10)
-			{
-			_putchar((num / 10) + '0');
+				_putchar((num / 10) + '0');
 			_putchar((num % 10) + '0');
-			}
-			else
-			_putchar((num % 10) + '0');
+			
 		}
 		_putchar('\n');
 	}
