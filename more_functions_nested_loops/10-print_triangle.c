cae4dd91eae65cef66d86b3cc588baeac4211d7c
juansechaux <juansechaux@hotmail.com>
2023-06-25 14:15:21 -0700

    10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - check the code
+ *@size: tamano del triangle
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+	int fila, colum, esp;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+		for (fila = 0; fila < size; fila++)
+		{
+			for (colum = size - fila; colum > 1; colum--)
+			{
+				_putchar('.');
+			}
+			for (esp = 0; esp <= fila; esp++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+}
+
