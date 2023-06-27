927149543c9baa1993e5f37610ff5343ecc820c0
juansechaux <juansechaux@hotmail.com>
2023-06-27 08:43:31 -0700

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * reset_to_98 - Change the value of n troug a pointer
+ *@n: variable to change
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
+
+
+
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+This is the contend of the proyect Pointers arrays strings.
+0-reset_to_98.c
+
