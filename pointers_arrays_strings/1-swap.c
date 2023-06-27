c7ac0608e981c833004623ab138ef4cdc9304d1e
juansechaux <juansechaux@hotmail.com>
2023-06-27 08:48:39 -0700

    1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * swap_int - Change the value of n troug a pointer
+ *@a: variable to change
+ *@b: Variable to change2
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+
+}
+
