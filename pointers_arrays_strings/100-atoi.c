23926ca719eb513e852d8a37222b24e83207a64b
juansechaux <juansechaux@hotmail.com>
2023-06-28 13:46:08 -0700

    100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,44 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * _atoi - Change the value of n troug a pointer src = dest[]
+ *@dest: pointer to paste the copy
+ *@src: pointer to copy
+ * Return: Always 0.
+ */
+
+int _atoi(char *s)
+{
+	int a, neg, num, val;
+
+	a = 0;
+	neg = 1;
+	num = 0;
+	val = 0;
+
+	while (s[a])
+	{
+		if (s[a] == 45)
+		{
+			neg *= -1;
+		}
+		while (s[a] >= 48 && s[a] <= 57)
+		{
+			val = 1;
+			num = (num * 10) + (s[a] - '0');
+			a++;
+		}
+
+		if (val == 1)
+		{
+			break;
+		}
+
+		a++;
+	}
+
+	num *= neg;
+	return (num);
+}
+
