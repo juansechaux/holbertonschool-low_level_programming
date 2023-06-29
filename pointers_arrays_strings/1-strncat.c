6e5830c31efd08665db337b90e676a49f092eacf
juansechaux <juansechaux@hotmail.com>
2023-06-29 08:16:09 -0700

    1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * _strncat - concatenate 2 strings whit n caracters
+ *@dest: string 1
+ *@src: string 2
+ *@n: numbers of caracters of the string 2
+ * Return: Always 0.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int id, is;
+
+	id = 0;
+	is = 0;
+
+	while (dest[id] != '\0')
+	{
+		id++;
+	}
+	for (is = 0; is < n; is++)
+	{
+		dest[id] = src[is];
+		id++;
+	}
+
+	dest[id] = '\0';
+
+	return (dest);
+
+}
+
+
+
