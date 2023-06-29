7bfcc58ed43fa20a931d09157dfa22dff1bc2fb6
juansechaux <juansechaux@hotmail.com>
2023-06-29 16:47:54 -0700

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -2,10 +2,9 @@
 #include"main.h"
 
 /**
- * _strncpy - concatenate 2 strings whit n caracters
- *@dest: string 1
- *@src: string 2
- *@n: numbers of caracters of the string 2
+ * _strcmp - concatenate 2 strings whit n caracters
+ *@s1: string 1
+ *@s2: string 2
  * Return: Always 0.
  */
 
