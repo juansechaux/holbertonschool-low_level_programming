beeb30d8a31eb6bf10c7c3bbc3999ed3fdd0b68a
juansechaux <juansechaux@hotmail.com>
2023-07-04 14:46:23 -0700

    1-print_rev_recursion.c

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ *_print_rev_recursion - funcion
+ *@s: la variable
+ *Return: Always 0.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
+
