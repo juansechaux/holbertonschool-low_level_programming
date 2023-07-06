4e9dc3e61245eec04cb69b0bc8ecf8eb89431ee1
juansechaux <juansechaux@hotmail.com>
2023-07-06 11:02:43 -0700

    4-pow_recursion.c

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ *_pow_recursion - funcion
+ *@x: base
+ *@y: exponente
+ *Return: Always 0.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y <= 1)
+	{
+		return;
+	}
+	return (x * _pow_recursion(x, (y - 1)));
+}
+
