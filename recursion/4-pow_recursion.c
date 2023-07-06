ec4d4f1ab46851aa2020ebb6850a4bb4c5e2550f
juansechaux <juansechaux@hotmail.com>
2023-07-06 11:09:53 -0700

    4-pow_recursion.c

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -14,13 +14,13 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	if (y == 0)
+	else if (y == 0)
 	{
 		return (1);
 	}
-	if (y <= 1)
+	else if (y <= 1)
 	{
-		return;
+		return (x);
 	}
 	return (x * _pow_recursion(x, (y - 1)));
 }
