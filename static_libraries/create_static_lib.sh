42058e0a03613b4ba31d8f5b8be7b43d9dd982d3
juansechaux <juansechaux@hotmail.com>
2023-07-07 09:32:31 -0700

    create_static_lib.sh

--- static_libraries/create_static_lib.sh
+++ static_libraries/create_static_lib.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# Get all .c files in the current directory
-c_files=$(ls *.c)
-# Compile each .c file into an object file
-for file in $c_files; do
-            gcc -c $file -o ${file%.c}.o
-    done
-    # Create the static library
-    ar rcs liball.a *.o
-    # Clean up the object files
-    rm *.o
-    echo "Static library liball.a created successfully."
+gcc -c *.c
+ar rc liball.a *.o
