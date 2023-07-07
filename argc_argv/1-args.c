1d7837fe5952a87f0a6d50d6e337b8c00fdfea16
juansechaux <juansechaux@hotmail.com>
2023-07-07 13:08:43 -0700

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - la funcion
+ *@argc: cont1
+ *@argv: cont2
+ *Return: Always 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
+
