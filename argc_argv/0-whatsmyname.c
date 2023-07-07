31e7eea4b329a3ffecbc123c081db40e3a71c304
juansechaux <juansechaux@hotmail.com>
2023-07-07 12:28:12 -0700

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - la funcion
+ *@argc: cont1
+ *@argv: cont2
+ *Return: Always 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	if (argc >= 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
+
---
+++ argc_argv/README.md
@@ -0,0 +1,2 @@
+This is the README file for the argc_argv proyect
+0-whatsmyname.c
