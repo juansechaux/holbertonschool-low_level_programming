fb9faa18aedfeb253a2d68a792b8ef73180c9991
juansechaux <juansechaux@hotmail.com>
2023-07-11 16:46:53 -0700

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,27 +17,53 @@ char *str_concat(char *s1, char *s2)
 	i = 0;
 	c = 0;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		if (s2 == NULL)
+		{
+			return (NULL);
+		}
+		while (s2[c] != '\0')
+		{
+			c++;
+		}
+		conca = malloc(sizeof(char) * (c + 1));
+		x = 0;
+		while (x <= c)
+		{
+			conca[x] = s2[x];
+			x++;
+		}
+		return (conca);
 	}
-
+	else if (s2 == NULL)
+	{
+		while (s1[i] != '\0')
+		{
+			i++;
+		}
+		conca = malloc(sizeof(char) * (i + 1));
+		t = 0;
+		while (t <= i)
+		{
+			conca[t] = s1[t];
+			t++;
+		}
+		return (conca);
+	}
 	while (s1[i] != '\0')
 	{
 		i++;
 	}
-
 	while (s2[c] != '\0')
 	{
 		c++;
 	}
-
 	conca = malloc(sizeof(char) * (i + c + 1));
 	for (t = 0; t < i; t++)
 	{
 		conca[t] = s1[t];
 	}
-
 	x = 0;
 	while (t <= (i + c))
 	{
