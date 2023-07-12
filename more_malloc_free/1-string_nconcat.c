3175a04e1f2903e9646639cae6879c6f8ffa7bd1
juansechaux <juansechaux@hotmail.com>
2023-07-12 15:19:06 -0700

    1-string_nconcat.c

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include"main.h"
+#include<stdlib.h>
+
+/**
+ *string_nconcat - funcion
+ *@s1: cadena1
+ *@s2: cadena2
+ *@n: cantidad de chars
+ *Return: Always 0.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int t, x, i = 0, c = 0;
+	char *conca;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i] != '\0')
+		i++;
+	while (s2[c] != '\0')
+		c++;
+	if (c <= n)
+	{
+		conca = malloc(sizeof(char) * (i + c + 1));
+		if (conca == NULL)
+			return (NULL);
+		for (t = 0; t < i; t++)
+			conca[t] = s1[t];
+		x = 0;
+		while (t < (i + c))
+		{
+			conca[t] = s2[x];
+			t++;
+			x++;
+		}
+		conca[t] = '\0';
+	}
+	else
+	{
+		conca = malloc(sizeof(char) * (i + n + 1));
+		if (conca == NULL)
+			return (NULL);
+		for (t = 0; t < i; t++)
+			conca[t] = s1[t];
+		x = 0;
+		while (t < (i + n))
+		{
+			conca[t] = s2[x];
+			t++;
+			x++;
+		}
+		conca[t] = '\0';
+	}
+	return (conca);
+}
+
