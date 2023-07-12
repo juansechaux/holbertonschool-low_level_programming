0ae7d50ebed9427c0f1a87ba0dc01fa28fe0a54c
juansechaux <juansechaux@hotmail.com>
2023-07-12 08:48:13 -0700

    prueba 2

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -15,9 +15,13 @@ int **alloc_grid(int width, int height)
 	int i, c;
 
 	matrix = (int **)malloc(height * sizeof(int *));
+	if (matrix == NULL)
+		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		matrix[i] = (int *)malloc(width * sizeof(int));
+		if (matrix == NULL)
+			return (NULL);
 		for (c = 0; c < width; c++)
 		{
 			matrix[i][c] = 0;
