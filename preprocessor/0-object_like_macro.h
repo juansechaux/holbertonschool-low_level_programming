976e88b83ff9549dc742a1925b281902d8cea256
juansechaux <juansechaux@hotmail.com>
2023-07-13 14:31:03 -0700

    0-object_like_macro.h

---
+++ preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef 0_OBJECT_LIKE_MACRO_H_
+#define 0_OBJECT_LIKE_MACRO_H_
+
+#define SIZE 1024
+
+#endif
---
+++ preprocessor/README.md
@@ -0,0 +1,2 @@
+This is the README.md file for the proyect preprocessor
+0-object_like_macro.h
