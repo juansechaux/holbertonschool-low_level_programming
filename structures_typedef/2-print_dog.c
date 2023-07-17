56cf55f5ed65787ee8c3f97eb55c9842e0b8ea92
juansechaux <juansechaux@hotmail.com>
2023-07-17 13:39:07 -0700

    2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -16,9 +16,9 @@ void print_dog(struct dog *d)
 			printf("(nil)\n");
 		else if (d->name != NULL)
 			printf("Name: %s\n", d->name);
-		if (d->age == '\0')
+		if (d->age < 0)
 			printf("(nil)\n");
-		else if (d->age != '\0')
+		else if (d->age >= 0)
 			printf("Age: %.6f\n", d->age);
 		if (d->owner == NULL)
 			printf("(nil)\n");
