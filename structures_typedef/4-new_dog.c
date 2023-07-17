3857d9c02028187418fdd0217ef289e7f071fd36
juansechaux <juansechaux@hotmail.com>
2023-07-17 15:31:38 -0700

    4-new_dog.c

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"dog.h"
+
+/**
+ * new_dog - prototype
+ *@name: dog name.
+ *@age: dog age.
+ *@owner: dog owner.
+ *Return: if its ok return d, else NULL
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog2;
+
+	new_dog2 = malloc(sizeof(dog_t));
+	if (new_dog2 == NULL)
+		return (NULL);
+	new_dog2->name = name;
+	new_dog2->age = age;
+	new_dog2->owner = owner;
+	return (new_dog2);
+}
