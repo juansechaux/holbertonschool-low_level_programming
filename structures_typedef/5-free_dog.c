5f2f21f65be44e9c71357f61081f199f7d9f069e
juansechaux <juansechaux@hotmail.com>
2023-07-17 16:52:58 -0700

    5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"dog.h"
+
+/**
+ * free_dog - prototype
+ *@d: dog name.
+ *Return: if its ok return d, else NULL
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+
+}
+
