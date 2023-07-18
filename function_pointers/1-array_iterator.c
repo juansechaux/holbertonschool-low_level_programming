6605be21b5f23c629ee878971aa7478888445fc4
juansechaux <juansechaux@hotmail.com>
2023-07-18 13:03:55 -0700

    1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -13,6 +13,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int c;
 
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+
 	for (c = 0; c < size; c++)
 	{
 		action(array[c]);
