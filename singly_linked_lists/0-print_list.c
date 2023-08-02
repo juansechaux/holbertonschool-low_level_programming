01ebe58269d47c6727441958f4738637e27771ea
juansechaux <juansechaux@hotmail.com>
2023-08-02 12:16:40 -0700

    0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+
+/**
+ * print_list - This is the function that prints all the elements of a list
+ * @h: pointer
+ * Return: the numbers of nodes
+ */
+
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+	const list_t *actual = h;
+
+	while (actual != NULL)
+	{
+		if (actual == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", actual->len, actual->str);
+		actual = actual->next;
+		i++;
+	}
+	return (i);
+}
+
---
+++ singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+This is the README.md file for the proyect singly_linked_lists
+0-print_list.c
