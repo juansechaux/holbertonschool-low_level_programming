f60526e17a988ae0e59a9b7a6d3e9a757f6c9cfb
juansechaux <juansechaux@hotmail.com>
2023-08-02 17:01:04 -0700

    ls

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+
+/**
+ * free_list - function that frees a list
+ * @head: points to head of the nodes
+ * Return: void
+ */
+
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
+
