6c7e7d5b598309e7f2e902809ae17aecd4955957
juansechaux <juansechaux@hotmail.com>
2023-08-09 13:00:43 -0700

    Prueba 2

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	bytes_read = read(fd1, buffer, 1024);
