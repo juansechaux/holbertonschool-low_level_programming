8918ec72d5462162311a9db1361e791c6d2e67e3
juansechaux <juansechaux@hotmail.com>
2023-08-09 12:56:19 -0700

    Prueba 1

---
+++ file_io/3-cp.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ *main - main function
+ *@argc: arguments
+ *@argv: files
+ *Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int fd1, fd2;
+	char buffer[1024];
+	ssize_t bytes_read, bytes_write;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		exit(99);
+	}
+	bytes_read = read(fd1, buffer, 1024);
+	bytes_write = write(fd2, buffer, bytes_read);
+	if (bytes_write == -1)
+	{
+		close(fd2);
+		exit(99);
+	}
+	if (close(fd1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", close(fd1));
+		exit(100);
+	}
+	if (close(fd2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", close(fd2));
+		exit(100);
+	}
+	return (0);
+
+}
