9b3914e788b8d8ef59baffa6049403cc1a078a4b
juansechaux <juansechaux@hotmail.com>
2023-08-10 16:41:59 -0700

    3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,13 +12,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = hash_djb2((const unsigned char *)key) % ht->size;
 
+	
+
 	hash_node_t *new_node = malloc(sizeof(hash_node_t));
 
 	if (new_node == NULL)
 		return (0);
 
-	new_node->key = (char *)key;
-	new_node->value = (char *)value;
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
 	new_node->next = NULL;
 
 	/*Agrega el nodo a la tabla hash (manejando colisiones si es necesario*/
