24d157ee5a649540fa3efdd459f8268d924cc8c3
juansechaux <juansechaux@hotmail.com>
2023-08-10 17:18:28 -0700

    prueba 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,7 +12,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = hash_djb2((const unsigned char *)key) % ht->size;
 
-	
+	if (ht == NULL)
+		return (0);
 
 	hash_node_t *new_node = malloc(sizeof(hash_node_t));
 
