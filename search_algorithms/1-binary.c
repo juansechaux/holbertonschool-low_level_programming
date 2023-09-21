51b0c2612a0413be5ae759d623a994b4769c6857
juansechaux <juansechaux@hotmail.com>
2023-09-21 16:56:52 -0700

    1-binary.c

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * linear_search - function that searches for a value in an array of integers
+ * binary_search - function that searches for a value in an array of integers
  * @array: pointer to the first element of the array to search in
  * @size: the number of elements in array
  * @value: the value to search for
